bf0920a95a3d51a42084ddd951a850398c16dcb0
Anton Wyrowski <anton.wyrowski@gmail.com>
2022-08-10 18:09:48 +0200

    fixed darkmode problem

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,23 @@ import 'react-toastify/dist/ReactToastify.css';
 import { createTheme, NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider } from 'next-themes';
 
+const lightTheme = createTheme({
+  type: 'light',
+  theme: {},
+});
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {},
+});
+
 const MVSTGithubApiChallenge = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider defaultTheme="system" attribute="class">
+    <ThemeProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{ light: lightTheme.className, dark: darkTheme.className }}
+    >
       <NextUIProvider>
         <Component {...pageProps} />
       </NextUIProvider>
